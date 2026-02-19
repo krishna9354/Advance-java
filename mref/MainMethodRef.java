cb1acb058b54a7118a801e45457c67f2454cdb11
Krishna Garg <gargkrishna9354@gmail.com>
2026-02-19 09:47:43 +0530

    MainMethodRef is added

--- mref/MainMethodRef.java
+++ mref/MainMethodRef.java
@@ -2,6 +2,8 @@ package mref;
 
 class Circle {
     //if the method will be static then, we have to change the method reference to Circle::area instead of c::area because static methods are associated with the class rather than an instance of the class.here the instance of the class is not required to invoke the static method.
+    
+
     double area(int r) {
         return Math.PI * r * r;
     }
@@ -18,8 +20,11 @@ public class MainMethodRef {
         Circle c = new Circle();
         Area ca = c::area; // Method reference to the instance method 'area' of the Circle class 
         // The method reference 'c::area' is used to create an instance of the Area functional interface, which can be invoked to calculate the area of a circle with a given radius.
-        //
         System.out.println("Area="+ca.getArea(34));
+        CircleInfo ci = Circle::new; // Method reference to the constructor of the Circle class
+        ci.getInfo("This is parameterized constructor"); // The method reference 'Circle::new' is used to create an instance of the CircleInfo functional interface, which can be invoked to create a new Circle object with a given string parameter.
+
+
 
     }
 
